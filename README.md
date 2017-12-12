35b00cd8f67207c0e079e1799fb63b4b945c92c6
Jed Perkins <jedp@alleninstitute.org>
2017-12-11 16:33:13 -0800

    Initial github pages documentation

--- README.md
+++
@@ -1,6 +0,0 @@
-AIBS Eye Tracking
-=================
-
-https://github.com/AllenInstitute/aibs.eye_tracking
-
-AIBS package for mouse eye tracking.
--- aibs/eye_tracking/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for AIBS Eye Tracking."""
-
-__author__ = """Jed Perkins"""
-__email__ = 'jedp@alleninstitute.org'
-__version__ = '0.1.0'
