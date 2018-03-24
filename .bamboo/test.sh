a7034145fa9804788aa3bc1d3540bd0b084fc1ab
Jed Perkins <jedp@alleninstitute.org>
2018-03-24 14:53:02 -0700

    Fix channel specification

--- .bamboo/test.sh
+++ .bamboo/test.sh
@@ -7,7 +7,7 @@ export CONDA_PREFIX=${CONDA_PREFIX:-}
 export CONDA_PS1_BACKUP=${CONDA_PS1_BACKUP:-}
 conda create -y -${bamboo_VERBOSITY} --clone ${bamboo_BASE_ENVIRONMENT} --prefix ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
 source activate ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
-conda install -y -${bamboo_VERBOSITY} -c scikit-image
+conda install -y -${bamboo_VERBOSITY} -c defaults scikit-image
 conda install -y -${bamboo_VERBOSITY} -c conda-forge opencv
 cd ${bamboo_build_working_directory}/${bamboo_CHECKOUT_DIRECTORY}
 pip install -r requirements.txt
